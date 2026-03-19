2c60bd34aa78b50589554264dff5b4bd63760388
joaopedroLZT <joao.rodrigues@grupolzt.com.br>
2026-03-19 14:43:52 -0300

    feat: implementado controle de acesso baseado em funções usando o decorador `Roles` e o `RolesGuard` para endpoints de usuário.

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -8,7 +8,10 @@ import {
   ResolveField,
 } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
+import { Role } from '@prisma/client';
 import { UserEntity } from '../common/decorators/user.decorator';
+import { Roles } from '../common/decorators/roles.decorator';
+import { RolesGuard } from '../common/guards/roles.guard';
 import { GqlAuthGuard } from '../auth/gql-auth.guard';
 import { UsersService } from './users.service';
 import { User } from './models/user.model';
@@ -16,7 +19,7 @@ import { ChangePasswordInput } from './dto/change-password.input';
 import { UpdateUserInput } from './dto/update-user.input';
 
 @Resolver(() => User)
-@UseGuards(GqlAuthGuard)
+@UseGuards(GqlAuthGuard, RolesGuard)
 export class UsersResolver {
   constructor(
     private usersService: UsersService,
@@ -26,6 +29,12 @@ export class UsersResolver {
   @Query(() => User)
   async me(@UserEntity() user: User): Promise<User> {
     return user;
+  }
+
+  @Query(() => [User])
+  @Roles(Role.ADMIN)
+  async allUsers() {
+    return this.prisma.user.findMany();
   }
 
   @UseGuards(GqlAuthGuard)
